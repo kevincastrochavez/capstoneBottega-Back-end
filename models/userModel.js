f6518a81da88ea3480e7686c514ee20b8c023484
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-01 16:24:16 -0500

    created catchAsync function for try and catch blocks

--- models/userModel.js
+++ models/userModel.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, "Please provide a valid email"],
   },
+  photo: String,
   password: {
     type: String,
     required: [true, "Please provide a password"],
