841c3a9a832c2310a14f43d9f4ff6bd1429e62c3
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-01 17:11:07 -0500

    connected database with express app

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const express = require("express");
 
+dotenv.config({ path: "./config.env" });
 const app = express();
 
 const products = JSON.parse(fs.readFileSync("./products.json"));
@@ -14,6 +17,19 @@ app.get("/api/v1/products", (req, res) => {
     },
   });
 });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Succesful Connection"));
 
 const port = 5000;
 app.listen(port, () => {
