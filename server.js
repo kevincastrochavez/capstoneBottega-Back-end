fb4d5361976352f4cb74c8da7feef04a06996c3f
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-02 16:44:48 -0500

    handled unhandled rejections

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const port = 5000;
 app.listen(port, () => {
   console.log(`Server started at port:${port}`);
 });
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection");
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
