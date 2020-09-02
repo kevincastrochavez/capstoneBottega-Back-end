78ba51b40576476f0c9b9bc83bd772a22d2d17f9
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-02 13:59:18 -0500

    implemented global error handling middleware

--- app.js
+++ app.js
@@ -16,11 +16,21 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "failed",
-    message: `Can not find ${req.originalUrl} on this server!`,
+  const err = new Error(`Can not find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 const DB = process.env.DATABASE.replace(
