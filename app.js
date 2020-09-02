0d4ab2621b9e506b5ecbd8acf009cbc3d221337c
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-02 13:52:02 -0500

    handled unhandled routes

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.use(express.json());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "failed",
+    message: `Can not find ${req.originalUrl} on this server!`,
+  });
+  next();
+});
+
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
