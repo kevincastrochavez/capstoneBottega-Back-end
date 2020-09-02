589b8b8288ddfccd1892b61a8686a9b4076730e3
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-02 17:14:25 -0500

    installed bcryptjs package to hash passwords and implemented the hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,6 +21,13 @@ const userSchema = new mongoose.Schema({
     minlength: 8,
     select: false,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
