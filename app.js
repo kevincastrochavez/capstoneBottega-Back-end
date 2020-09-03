c376b743dde77356b219ed6830bf24c73189cd77
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-03 12:30:46 -0500

    sent jwt via cookie and set security http headers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,8 +9,10 @@ const productRouter = require("./routes/productsRoutes");
 
 const app = express();
 
+app.use(helmet());
+
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
