8310e4e84994e595757b5bcc94518a5ba24eed2e
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-03 12:35:48 -0500

    installed xss and mongoSanitize for data sanitization

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,7 +14,12 @@ const app = express();
 app.use(helmet());
 
 app.use(morgan("dev"));
+
 app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
