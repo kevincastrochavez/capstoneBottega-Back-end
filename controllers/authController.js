f4377c18437319b66097222676604b4bad660a6d
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-03 11:27:57 -0500

    setup password reset functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../catchAsync");
@@ -84,4 +85,32 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
       new AppError("There was an error sending the email. Try again later", 500)
     );
   }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new AppError("Token is invalid or has expired", 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    token,
+  });
 });
