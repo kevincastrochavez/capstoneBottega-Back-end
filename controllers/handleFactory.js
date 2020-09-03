97f0a56acf73fbceb58085d42547fa9d3bb6ae21
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-09-03 13:21:24 -0500

    factory function for getting users

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -31,3 +31,32 @@ exports.updateOne = (Model) =>
       },
     });
   });
+
+exports.getOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findById(req.params.id);
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.find();
+
+    res.status(200).json({
+      status: "success",
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,7 @@
 const User = require("../models/userModel");
-const catchAsync = require("../catchAsync");
 const factory = require("./handleFactory");
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  res.status(200).json({
-    status: "success",
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
+exports.getAllUsers = factory.getAll(User);
 exports.createUser = factory.createOne(User);
-
-exports.getUser = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      user,
-    },
-  });
-});
-
+exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User);
